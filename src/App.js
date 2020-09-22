3ba0dc0e5ff6da98676910b3a037a850fe0fc1f6
Prajapati, Viral Kartikbhai <vkprajap@iu.edu>
2020-09-22 15:43:09 -0400

    testing Auth0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
+import { NavBar, Footer, Loading, PrivateRoute } from "./components";
+import { Home, Profile, ExternalApi } from "./views";
+
+import "./App.css";
+
+const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="d-flex flex-column h-100">
+      <NavBar />
+      <Container className="flex-grow-1 mt-5">
+        <Switch>
+        <Route path="/" exact component={Home} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/external-api" component={ExternalApi} />
+        </Switch>
+      </Container>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
