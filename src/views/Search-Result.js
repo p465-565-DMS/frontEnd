c95770ea34f02671940662c9d126f52c1a9dbe68
laichunx <48215216+laichunx@users.noreply.github.com>
2020-11-03 17:13:19 +0800

    Search Result page completed

--- src/views/Search-Result.js
+++ src/views/Search-Result.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Divider from '@material-ui/core/Divider';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 // reactstrap components
 import {
   Button,
@@ -14,37 +15,59 @@ import {
   Row,
   Col,
 } from "reactstrap";
+import { Container } from "@material-ui/core";
+
+
+const divStyle = {
+    //border: '2px solid black',
+    //borderRadius: '5px'  
+};
+
+const selectButton = () => (
+    <Route render={({history}) => (
+      <Button
+        type='button'
+        onClick={() => {
+            history.push('/search-result');
+        }}
+        aria-label="Check"
+        size="sm"
+      >
+        Select
+      </Button>
+    )} />
+)
 
 export default function SearchResult(props) {
     return (
       <>
-        <div className="content mt-5 pt-5">
+        <Container className="content mt-5 pt-5">
              <Row>
              <Col md="2"/>
             <Col md="8">
             <Row>
-                <h2>Search Result</h2>
+                <Container style={divStyle}>
+                    <h2>Search Result</h2>
+                    <Row>
+                        <Col md="4 pl-4">
+                            <span>From:&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                            <span>Jordan Ave Bloomington</span>
+                        </Col>
+                        <Col md="4 pl-4">
+                            <span>To:&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                            <span>E Rogers Rd Bloomington</span>
+                        </Col>
+                        <Col md="4 pl-4">
+                            <span>Date:&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                            <span>12/31/2020</span>
+                        </Col>
+                    </Row>
+                </Container>
             </Row>
-            <Row>
-                <Col md="4">
-                    <span>From:&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <span>Jordan Ave Bloomington</span>
-                </Col>
-                <Col md="4">
-                    <span>To:&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <span>E Rogers Rd Bloomington</span>
-                </Col>
-                <Col md="4">
-                    <span>Date:&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <span>12/31/2020</span>
-                </Col>
-            </Row>
-            <Divider variant="middle"/>
-              <Card className="d-flex flex-column mt-5">
+            <Divider className="mt-2" variant="middle"/>
+            <Card className="d-flex flex-column mt-5">
                 <Row>
-                    <Col md="1">
-                    </Col>
-                    <Col md="2">
+                    <Col md="4">
                         <div className="p-2 col-example text-left">Company</div>
                     </Col>
                     <Col md="4">
@@ -54,16 +77,58 @@ export default function SearchResult(props) {
                         <div className="p-2 col-example text-left">Price</div>
                     </Col>
                 </Row>
-            </Card>
-            <Card className="d-flex flex-column-reverse">
-                <div className="p-2 col-example text-left">Flex item 1</div>
-                <div className="p-2 col-example text-left">Flex item 2</div>
-                <div className="p-2 col-example text-left">Flex item 3</div>
+                <Divider variant="middle"/>
+                
+                <Row>
+                    <Col md="4">
+                        <div className="p-2 col-example text-left">Hermes</div>
+                    </Col>
+                    <Col md="4">
+                        <div className="p-2 col-example text-left">01/01/2020</div>
+                    </Col>
+                    <Col md="2">
+                        <div className="p-2 col-example text-left">$15</div>
+                    </Col>
+                    <Col md="2">
+                        <button className="m-1">select</button>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col md="4">
+                        <div className="p-2 col-example text-left">UPS</div>
+                    </Col>
+                    <Col md="4">
+                        <div className="p-2 col-example text-left">02/01/2020</div>
+                    </Col>
+                    <Col md="2">
+                        <div className="p-2 col-example text-left">$17</div>
+                    </Col>
+                    <Col md="2">
+                        <button className="m-1">select</button>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col md="4">
+                        <div className="p-2 col-example text-left">USPS</div>
+                    </Col>
+                    <Col md="4">
+                        <div className="p-2 col-example text-left">03/01/2020</div>
+                    </Col>
+                    <Col md="2">
+                        <div className="p-2 col-example text-left">$20</div>
+                    </Col>
+                    <Col md="2">
+                        <button className="m-1">select</button>
+                    </Col>
+                </Row>
+                
             </Card>
             </Col>
             <Col md="2"/>
           </Row>
-        </div>
+        </Container>
       </>
     );
 }
